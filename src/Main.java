195d78ad1b38298012036bce933e9f6ec4d8c781
Peonsson <xsnudelx@hotmail.com>
2015-12-10 15:11:46 +0100

    can now handle multiple clients

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
+/**
+ * SuperSimpleServer is a TCP server who accepts a client connection and writes clients message to a file.
+ * Made by Peonsson 2015-12-10.
+ */
 public class Main {
 
     private static final int PORT_NUMBER = 1337;
@@ -9,26 +14,31 @@ public class Main {
     public static void main(String[] args) {
 
         ServerSocket serverSocket;
-        File file = new File("/Users/Peonsson/myData.txt");
         FileWriter fileWriter;
 
         try {
-            fileWriter = new FileWriter(file);
             serverSocket = new ServerSocket(PORT_NUMBER);
-            System.out.println("listening at.. " + serverSocket.getInetAddress() + ":" + serverSocket.getLocalPort());
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("got client! ");
-            System.out.println(clientSocket.getInetAddress());
-            System.out.println(clientSocket.getPort());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String str = "";
-            while((str = reader.readLine()) != null) {
-                System.out.println("got data: " + str);
-                fileWriter.append(str);
-            }
-            fileWriter.close();
-            System.out.println("returning..");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                String date = new Date().toString();
+                File file = new File("/Users/Peonsson/androidproject3b/" + date + ".txt");
+                fileWriter = new FileWriter(file);
+                System.out.println("got client! ");
 
+//                System.out.println(clientSocket.getInetAddress());
+//                System.out.println(clientSocket.getPort());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String str;
+                while ((str = reader.readLine()) != null) {
+                    System.out.println("got data: " + str);
+                    fileWriter.append(str);
+                }
+
+                fileWriter.close();
+                System.out.println("returning..");
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
